de531897a1f2e48c83c006c9915292a0f5b984e8
Michael Beckler <michael.beckler@ge.com>
2019-03-19 11:31:38 -0400

    updates

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-
-
 # build the project -> outputs to public folder
 hugo --minify --buildFuture
 
@@ -15,6 +13,5 @@ git add -f public && git commit -m "Update content"
 
 # push the public folder ONLY to the gh-pages branch
 git subtree push --prefix public origin gh-pages --squash
-
 
 echo -e "\033[0;32mFinished deploying.\033[0m"
