aad6b8c28f9f2985577bfef3f3f23ba311670e07
Michael Beckler <michael.beckler@ge.com>
2019-03-19 11:46:43 -0400

    update deploy stratagy

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# pull in any changes from the gh-pages branch
-git subtree pull --prefix public origin gh-pages
-
 # build the project -> outputs to public folder
 hugo --minify --buildFuture
 
-# commit the public folder
-git add -f public && git commit -m "Update content"
+# setup worktree
+git worktree add public gh-pages
 
-# push the public folder ONLY to the gh-pages branch
-git subtree push --prefix public origin gh-pages --squash
+# commit the public folder
+git add public && git commit -m "Update content"
+
+# push to the gh-pages branch
+git push origin gh-pages
 
 echo -e "\033[0;32mFinished deploying.\033[0m"
